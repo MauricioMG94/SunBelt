a36c1430666782a181ed283daa460d243c0487c9
Mauricio Martinez <ivanm.martinezg@gmail.com>
2024-03-23 21:19:40 -0500

    Configuración de SpringBoot para el proyecto.

---
+++ src/main/java/com/sunbelt/consultas_be/ConsultasBeApplication.java
@@ -0,0 +1,13 @@
+package com.sunbelt.consultas_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultasBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultasBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunbelt/consultas_be/ConsultasBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunbelt.consultas_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultasBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
